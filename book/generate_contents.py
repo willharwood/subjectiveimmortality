aa7972b94bf5868de199e6f0dc868966cafcc691
Will Harwood <harwood.will@gmail.com>
2026-04-21 04:50:52 +0100

    auto

--- book/generate_contents.py
+++ book/generate_contents.py
@@ -2,7 +2,7 @@ with open('book.html', 'r') as file:
     print_abstract = False
     for line in file:
         if "<h1" in line:
-            print(line.replace("h1", "a").replace('id="', 'href="book.html#'))
+            print("<br>" + line.replace("h1", "a").replace('id="', 'href="book.html#'))
         if '<p class="abstract"' in line:
             print_abstract=True
         elif '</p>' in line and print_abstract:
